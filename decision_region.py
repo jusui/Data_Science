37557706627ca0b6337bed48c72daf91d055e177
JunyaUSUI <junya.usui.0714@gmail.com>
2017-11-17 15:28:59 +0900

    Branch:Sub

--- decision_region.py
+++ decision_region.py
@@ -1,10 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 from matplotlib.colors import ListedColormap
 from perceptron import Perceptron
-import pymlb_ch02
 
-def plot_decision_region(X, y, classifier, resolution = 0.02):
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header = None)
+print(df.tail)
+
+def decision_region(X, y, classifier, resolution = 0.02):
+    # Vars [1 : 100]
+    y = df.iloc[0:100, 4].values
+    y = np.where( y == 'Iris-setosa', -1, 1 )
+    print(y)
+    
+    # [1:100], column(1:3)
+    X = df.iloc[0:100, [0, 2]].values
+    print(X)
+
+    # Plot setosa
+    plt_setosa = plt.scatter(X[:50, 0], X[:50, 1], color = 'red', marker = 'o', label = 'setosa')
+
+    # Plot versicolor
+    plt_versicolor = plt.scatter(X[50:100, 0], X[50:100, 1], color = 'blue', marker = 'o', label = 'versicolor')
+
+    # Object generator && Fit training model
+    ppn = Perceptron(eta = 0.1, n_iter = 10)
+    ppn.fit(X, y)
 
     # Marker & Color map
     markers = ('s', 'x', 'o', '^', 'v')
@@ -34,13 +55,26 @@ def plot_decision_region(X, y, classifier, resolution = 0.02):
 
     # Plot class sample
     for idx, cl in enumerate(np.unique(y)):
-        plt.scatter(x = X[y == cl, 0], y = X[y == cl, 1], alpha = 0.8, c = cmap(idx),
+        plt.scatter(x = X[y == cl, 0], y = X[y == cl, 1], \
+                    alpha = 0.8, c = cmap(idx), \
                     marker = markers[idx], label = cl)
 
+if __name__ == '__main__':
+    # Vars [1 : 100]
+    y = df.iloc[0:100, 4].values
+    y = np.where( y == 'Iris-setosa', -1, 1 )
+    print(y)
     
-# # plot decision region
-plot_decision_region(X, y, classifier = ppn)
-plt.xlabel('sepal length [cm]')
-plt.ylabel('petal length [cm]')
-plt.legend(loc = 'upper left')
-plt.show()
+    # [1:100], column(1:3)
+    X = df.iloc[0:100, [0, 2]].values
+    print(X)
+
+    # Object generator && Fit training model
+    ppn = Perceptron(eta = 0.1, n_iter = 10)
+    ppn.fit(X, y)
+
+    decision_region(X, y, classifier = ppn)
+    plt.xlabel('sepal length [cm]')
+    plt.ylabel('petal length [cm]')
+    plt.legend(loc = 'upper left')
+    plt.show()
--- pymlb_ch02.py
+++ pymlb_ch02.py
@@ -3,38 +3,35 @@ import matplotlib.pyplot as plt
 import numpy as np
 from perceptron import Perceptron
 
-df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header = None)
-print(df.tail)
+def iris_dataset(X, y):
+    df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header = None)
+    print(df.tail)
 
-# Vars [1 : 100]
-y = df.iloc[0:100, 4].values
-y = np.where( y == 'Iris-setosa', -1, 1 )
-print(y)
+    # Vars [1 : 100]
+    y = df.iloc[0:100, 4].values
+    y = np.where( y == 'Iris-setosa', -1, 1 )
+    print(y)
 
-# [1:100], column(1:3)
-X = df.iloc[0:100, [0, 2]].values
-print(X)
+    # [1:100], column(1:3)
+    X = df.iloc[0:100, [0, 2]].values
+    print(X)
 
-# Plot setosa
-plt_setosa = plt.scatter(X[:50, 0], X[:50, 1], color = 'red', marker = 'o', label = 'setosa')
+    # Plot setosa
+    plt_setosa = plt.scatter(X[:50, 0], X[:50, 1], color = 'red', marker = 'o', label = 'setosa')
 
-# Plot versicolor
-plt_versicolor = plt.scatter(X[50:100, 0], X[50:100, 1], color = 'blue', marker = 'o', label = 'versicolor')
+    # Plot versicolor
+    plt_versicolor = plt.scatter(X[50:100, 0], X[50:100, 1], color = 'blue', marker = 'o', label = 'versicolor')
 
-plt.xlabel('sepal length [cm]')
-plt.ylabel('petal length [cm]')
-plt.legend(loc = 'upper left')
-# plt.show()
+    # plt.xlabel('sepal length [cm]')
+    # plt.ylabel('petal length [cm]')
+    # plt.legend(loc = 'upper left')
+    # plt.show()
 
+    # Object generator && Fit training model
+    ppn = Perceptron(eta = 0.1, n_iter = 10)
+    ppn.fit(X, y)
 
-# Object generator
-ppn = Perceptron(eta = 0.1, n_iter = 10)
-
-# Fit a training model
-ppn.fit(X, y)
-
-# Plot
-plt.plot(range(1, len(ppn.errors_) + 1 ), ppn.errors_, marker = 'o' )
-plt.xlabel('Epochs')
-plt.ylabel('Number of misclassfications')
-plt.show()
+    # plt.plot(range(1, len(ppn.errors_) + 1 ), ppn.errors_, marker = 'o' )
+    # plt.xlabel('Epochs')
+    # plt.ylabel('Number of misclassfications')
+    # plt.show()
