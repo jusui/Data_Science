4d6b96da8ddb4f28beb89cb4a3279748b9b4d3ff
JunyaUSUI <junya.usui.0714@gmail.com>
2017-11-20 18:05:23 +0900

    Native bayes class, run, tsv_file

---
+++ nbayes_1/run_nbayes1.py
@@ -0,0 +1,28 @@
+"""
+NativeBayes1 test runner
+
+"""
+import numpy as np
+from nbayes1 import NativeBayes1
+
+# load data
+data = np.genfromtxt('vote_filled.tsv', dtype = int)
+
+# split data
+X = data[:, :-1]
+y = data[:, -1]
+
+
+# learn model
+clr = NativeBayes1()
+clr.fit(X, y)
+
+
+# test model
+predict_y = clr.predict(X[:10, :])
+
+# print results
+for i in range(10):
+    print(i, y[i], predict_y[i])
+
+    
