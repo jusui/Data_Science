0b3227876d98bfbbc842d11b9a46cbd306b8cac3
JunyaUSUI <junya.usui.0714@gmail.com>
2017-11-26 01:00:29 +0900

    np.reshape()(a, newshape) Gives a new shape to an  array without changing its data.

--- nbayes_1/ndim_shape.py
+++ nbayes_1/ndim_shape.py
@@ -5,12 +5,12 @@ def ndim_shape():
     check n dimension
     """
 
-    a = np.array([[1, 2], [2, 1]])
+    a = np.array( [[1, 3], [2, 1]] )
     print("a =", a)
     print("a.T =", a.T)
-
     print("a.transpose() =", a.transpose())
 
+    print("Let's transpose 1-d array")
     b = np.array([10, 20])
     print("b =", b)
     print("b.T =", b.T)    
@@ -28,7 +28,17 @@ def ndim_shape():
     print("d = b[np.newaxis, :] =", d)
     print("d.ndim =", d.ndim)
     print("d.shape =", d.shape)
-    
+
+    print("c.T =", c.T)
+    print("d.T =", d.T)
+
+    e = np.array([[1, 2, 3], [2, 4, 6]])
+    print("e.shape =", e.shape)
+    print("e[np.newaxis, :, :].shape =", e[np.newaxis, :, :].shape)
+    print("e[:, np.newaxis, :].shape =", e[:, np.newaxis, :].shape)
+    print("e[:, :, np.newaxis].shape =", e[:, :, np.newaxis].shape)
+    print("e[np.newaxis, :, np.newaxis, :].shape =", e[np.newaxis, :, np.newaxis, :].shape)
+   
 
 if __name__ == '__main__':
     ndim_shape()
