7d5c43226de398d176c2fc7a6a89e4e91e6c0251
JunyaUSUI <junya.usui.0714@gmail.com>
2017-12-01 17:40:16 +0900

    histogram, gaussian, hypothesis_test

--- kernel_density_est2.py
+++ kernel_density_est2.py
@@ -8,8 +8,19 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-dataset2 = randn(100)
-sns.kdeplot(dataset2, label = 'KDE')
+dataset = randn(100)
+sns.kdeplot(dataset)
+
+sns.rugplot(dataset, color = 'black')
+for bw in np.arange(0.5, 2, 0.25):
+    sns.kdeplot(dataset, bw = bw, label = bw)
+
+plt.figure()
+
+kernel_options = ['biw', 'cos', 'epa', 'gau', 'tri', 'triw']
+for kern in kernel_options:
+    sns.kdeplot(dataset, kernel = kern, label = kern)
 
 plt.show()
-plt.legend()
+# plt.legend()
+
