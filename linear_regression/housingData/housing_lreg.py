fd5d5104e653429227063162c773f5174b315fcf
JunyaUSUI <junya.usui.0714@gmail.com>
2017-12-17 20:07:10 +0900

    Update lreg. sns.heatmap, sns.clustermap. Lec14.

--- linear_regression/housingData/housing_lreg.py
+++ linear_regression/housingData/housing_lreg.py
@@ -13,6 +13,9 @@ print(df.head())
 
 x_value = df[['RM']].values
 y_value = df[['MEDV']].values
+age = df[['AGE']].values
+crim = df[['CRIM']].values
+
 print("Average room # ")
 print(x_value)
 print("room = 9")
@@ -27,25 +30,42 @@ model = LinearRegression()
 # train model
 model.fit(x_value, y_value)
 
-""" Lect14 """
-# 切片
-print(model.intercept_)
-# print('intercept_ = {:0.2f}'.format(model.intercept_))
-# 傾きの係数
-print(model.coef_)
-# print('coef_ = {:0.2f}'.format(len(model.coef_)))
-
-print("Room == 9 の場合")
-print( (model.intercept_ * 9) + model.coef_ )
-
 # plot model
 plt.scatter(x_value, y_value, c = 'blue')
 
 # 回帰線
 plt.plot(x_value, model.predict(x_value), c = 'red')
 
+""" Lect14 : 2つの方法で試行 """
+# ①線形関数から計算
+print("傾きの係数")
+print(model.coef_)
+print("切片")
+print(model.intercept_)
+print("Room == 9 の場合")
+print( 9 * (abs(model.coef_) ) + model.intercept_ )
+
+# ②作成したモデルから計算
+# https://www.udemy.com/neuralnet/learn/v4/questions/2665742
+plt.plot(9, model.predict(9), 'ro')
+print(model.predict(9))
+
 # コスメ
 plt.xlabel('average room #')
 plt.ylabel("Price[$1000's]")
+plt.figure()
+
+plt.scatter(age, y_value, c = 'blue')
+model.fit(age, y_value)
+plt.plot(age, model.predict(age), c = 'red')
+plt.xlabel('Age')
+plt.ylabel("Price[$1000's]")
+plt.figure()
+
+# sns.heatmap(df, annot = True)
+sns.lmplot("AGE", "MEDV", df, x_estimator = np.mean)
+
+sns.clustermap(df)
+
 
 plt.show()
