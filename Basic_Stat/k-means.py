210015d78b143c0e6fbe261b785d52470c1d76f8
JunyaUSUI <junya.usui.0714@gmail.com>
2017-12-25 22:02:49 +0900

    K-Means, https://pythondatascience.plavox.info/scikit-learn/%e3%82%af%e3%83%a9%e3%82%b9%e3%82%bf%e5%88%86%e6%9e%90-k-means

---
+++ Basic_Stat/k-means.py
@@ -0,0 +1,53 @@
+# coding: utf-8
+from numpy import random, array
+
+"""
+clustering した収入と年齢のランダムなFake data
+
+"""
+# create fake income/age clusters for N people in k clusters
+def createClusteredData(N, k):
+    random.seed(10)
+    pointsPerCluster = float(N) / k
+    X = []
+
+    for i in range(k):
+        incomeCentroid = random.uniform(20000.0, 200000.0)
+        ageCentroid    = random.uniform(20.0, 70.0)
+
+        for j in range(int(pointsPerCluster)):
+            X.append( [random.normal(incomeCentroid, 10000.0), random.normal(ageCentroid, 2.0)] )
+
+    X = array(X)
+
+    return X
+    
+"""
+K-Means でClusterの再設定し，教師なし学習
+"""
+
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import scale
+from numpy import random, float
+
+# 100 人-> 5 Cluster
+data = createClusteredData(100, 5)
+
+# KMeans(5)
+model = KMeans(n_clusters = 5)
+
+# Note I'm scaling the data to normalize it! Important for good results.
+model = model.fit(scale(data))
+
+# We can see the clusters each data point was assinged to
+# labels_ :: Labels of each point
+print("model.labels_ =", model.labels_)
+print("model.inertia_ = Sum of squared distances of samples to their closest cluster center.")
+print(model.inertia_)
+print("model.cluster_centers_ =", model.cluster_centers_)
+
+# plot
+plt.figure(figsize = (8, 6))
+plt.scatter(data[:, 0], data[:, 1], c = model.labels_.astype(float))
+plt.show()
