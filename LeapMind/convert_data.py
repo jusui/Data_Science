db1e0675d33876327e7ffd50df5afad1ee544baa
JunyaUSUI <junya.usui.0714@gmail.com>
2018-01-31 04:03:35 +0900

    make convert_data.py

--- LeapMind/convert_data.py
+++ LeapMind/convert_data.py
@@ -2,32 +2,25 @@
 import numpy as np
 
 def convert_CSV(image, label, output, dim):
-    image_file = open(image, 'rb')
-    writer = open(output, 'w')
-    labels = open(label, 'rb')
-
-    image_file.read(16)
-    print(image_file)
-    labels.read(8)
-    print(labels)
-    
-    images = []
-    for i in range(dim):
-        image = [ord(labels.read(1))]
-        for j in range(28*28):
-            image.append(ord(image_file.read(1)))
-        images.append(image)
-
-    for image in images:
-        print(image)
-        writer.write(",".join(str(pix) for pix in image)+"\n") 
-    image_file.close()
-    writer.close()
-    labels.close()
+    with open(image, 'rb') as image_file, open(label, 'rb') as labels, open(output, 'w') as writer:
+        image_file.read(16)     
+        labels.read(8)
+        
+        images = []
+        for i in range(dim):
+            image = [ord(labels.read(1))] #str -> integer (code point), label
+            for j in range(28*28):
+                image.append(ord(image_file.read(1)))
+            images.append(image)
+                
+        for image in images:
+            # print(image)
+            writer.write(",".join(str(pixel) for pixel in image)+"\n")
 
 if __name__ == '__main__':
-    convert_CSV("train-images-idx3-ubyte", "train-labels-idx1-ubyte",
-            "mnist_train_100.csv", 100) # dim = 60000
-    convert_CSV("t10k-images-idx3-ubyte", "t10k-labels-idx1-ubyte",
-            "mnist_test_10.csv", 10) # dim = 10000
+    convert_CSV("./dataset/train-images-idx3-ubyte", "./dataset/train-labels-idx1-ubyte",
+                "mnist_train_100.csv", 100) # dim = 60000
+    
+    convert_CSV("./dataset/t10k-images-idx3-ubyte", "./dataset/t10k-labels-idx1-ubyte",
+                "mnist_test_10.csv", 10) # dim = 10000
 
