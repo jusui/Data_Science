ead575f58cb70c9c0f8ddb8e881323eea078832e
JunyaUSUI <junya.usui.0714@gmail.com>
2018-02-14 23:26:18 +0900

    no message

--- Basic_python/mit09.py
+++ Basic_python/mit09.py
@@ -58,11 +58,90 @@ def squareRootBi(x, epsilon):
     return ans
 
 
+# 全近記法:asymptotic notation
+def f(x):
+    """ xを　正, int """
+    ans = 0
+    for i in range(1000):
+        ans += 1
+    print('Number of additions so far', ans)
+    # xの時間を要するループ
+    for i in range(x):
+        ans += 1
+    print('Number of additions so far', ans)
+    # x**2の時間を要する入れ子グループ
+    for i in range(x):
+        for j in range(x):
+            ans += 1
+            ans += 1
+    print('Number of additions so far', ans)
+    return ans
+
+
+# 9.3
+def intToStr(i):
+    """ int iの値を10進数で表す文字列を返す """
+    digits = '0123456789'
+    if i == 0:
+        return '0'
+    result = ''
+    while i > 0:
+        result = digits[i%10] + result
+        i = i // 10
+    return result
+
+# 9.3.3 線形計算時間
+def addDigits(s):
+    """ string s かつ各文字が10進数の数字とする，sの各数字の和を整数として返す """
+    val = 0
+    for c in s:
+        val += int(c)
+    return val
+
+def factorial(x):
+    """ xを正のint型とする，x!を返す """
+    if x == 1:
+        return 1
+    else:
+        return x*factorial(x-1)
+
+# 9.3.4 対数線形計算時間(e.f. merge sort) O(nlogn)
+# 9.3.5 多項式計算問題
+def isSubset(L1, L2): # O(len(L1)) ×O(len(L2))
+    """ list L1, L2. L1の各要素がL2にもあればTrue，なければFalseを返す """
+    for e1 in L1:
+        matched = False
+        for e2 in L2:
+            if e1 == e2:
+                matched = True
+                break
+        if not matched:
+            return False
+    return True
+
+
+def intersect(L1, L2):
+    """ list L1, L2. 重複のないリストを返す．
+    L1とL2の共通点からなる，重複リストを返す．"""
+    tmp = []
+    for e in L1:
+        for e2 in L2:
+            if e1 == e2:
+                tmp.append(e1)
+                break
+
+    # 重複のないリストを構築する
+    result = []
+    for e in tmp:
+        if e not in result:
+            results.append(e)
+    
+        
+
+
 
 if __name__ == '__main__':
-    # L = random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9])
-    # LS = linearSearch(L, 3)
-    # print(LS)
-    
-    F = fact(2)
+    F = f(1000) # f(10)
     print(F)
+
+    print(isSubset([1, 20, 34], [20, 103, 120]))
