dda53e6090a2c92b5157a41e83ddb0e12677e922
JunyaUSUI <junya.usui.0714@gmail.com>
2018-04-01 23:10:19 +0900

    no message

--- Basic_python/mit12.py
+++ Basic_python/mit12.py
@@ -1,12 +1,17 @@
 # coding: utf-8
 from mit09 import getPowerset
 
+"""
+12. Knapsack problem & Graph optimization
+"""
+
+# 12.1 Item(品物) class
 class Item(object):
-    # Item class
+    
     def __init__(self, n, v, w):
-        self.name = n
-        self.value = v
-        self.weight = w
+        self.name = n   # 名前
+        self.value = v  # 価格
+        self.weight = w # 重さ
 
     def getName(self):
         return self.name
@@ -32,11 +37,14 @@ def density(item):
     return item.getValue() / item.getWeight()
 
 
+# 12.2 keyFunction引数に紐づく
 def greedy(items, maxWeight, keyFunction):
     """ items はリスト，maxWeight >= 0とし，
-    keyFunctionはitemsの要素を数にマップする """
-    
-    itemsCopy = sorted(items, key = keyFunction, reverse = True)
+    keyFunctionはitemsの要素を数にマップする.
+    t_calc ~ O(nlogn) """
+
+    # sorted()で新しいリストを作り大きい順に並べる
+    itemsCopy = sorted(items, key = keyFunction, reverse = True) 
     result = []
     totalValue, totalWeight = 0.0, 0.0
     for i in range(len(itemsCopy)):
@@ -47,6 +55,8 @@ def greedy(items, maxWeight, keyFunction):
             
     return (result, totalValue)
 
+
+# 12.3 品物(item)リストを作り，リストの異なった順序を用いてgreedy()をテストするtestGreedysを定義
 def buildItems():
     names = ['clock', 'painting', 'radio', 'vase', 'book', 'computer']
     values = [175, 90, 20, 50, 10, 200]
@@ -76,13 +86,24 @@ def testGreedys(maxWeight = 20):
 """
 0/1 Knapsack problem
 
-1．すべての品物の組み合わせを列挙する。品物集合のすべての部分集合を作る。べき集合。
-2．重量制限を超えるような品物の組み合わせを取り除く。
-3．残された組み合わせの内，総価値が最も大きいものを選ぶ。
+1．それぞれの品物は<価値，重さ>のペアで表される
+2．ナップサックはwを越えない総重量の品物を収容できる
+3．長さがnのベクトルIは，収容可能な品物の集合を表す
+4．長さがnのベクトルVは，それぞれの品物の集合あきすによって盗まれるかどうかを示すのに使われる。
+もしV[i] = 1ならば，品物I[i]は盗まれるもの，もしV[i] = 0ならば品物I[i]は盗まれないとする。
+5．以下の式を最大化するベクトルVを見つける
+
+0 <= i < n-1
+sum(V[i] * I[i].value)
 """
 
 def chooseBest(pset, maxWeight, getVal, getWeight):
 
+    """
+    1．すべての品物の組み合わせを列挙する。つまり，品物集合のすべての部分集合(べき集合)を作る。
+    2．重量制限を超えるような品物の組み合わせを取り除く。
+    3．残された組み合わせの内，総価値が最も大きいものを選ぶ。
+    """
     bestVal = 0.0
     bestSet = None
     for items in pset:
